028dc4f768ea048ea7da185eda076828d5efa4ea
Jafar <hlpntng@gmail.com>
2025-12-07 03:42:23 +0700

    Add Sepeda class extending Kendaraan

---
+++ Sepeda.java
@@ -0,0 +1,19 @@
+package RentalKendaraan;
+
+public class Sepeda extends Kendaraan {
+    private String jenisSepeda;
+
+    public Sepeda(String merk, String model, int tahunProduksi, String jenisSepeda) {
+        super(merk, model, tahunProduksi);
+        this.jenisSepeda = jenisSepeda;
+    }
+
+    public String getJenisSepeda() {
+        return jenisSepeda;
+    }
+
+    @Override
+    public String detailInformation() {
+        return "Sepeda: " + super.detailInformation() + ", Jenis: " + jenisSepeda;
+    }
+}
